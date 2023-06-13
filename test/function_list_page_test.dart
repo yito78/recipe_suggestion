c7dc46cbffb6627849b5dc18894e217fbe6d192e
yosukeito <yosuke.ito@uniadex.co.jp>
2023-06-13 16:09:40 +0900

    issue #38 不要テストファイル、テストコード削除

--- test/function_list_page_test.dart
+++ test/function_list_page_test.dart
@@ -2,28 +2,10 @@ import "package:firebase_core/firebase_core.dart";
 import 'package:flutter/material.dart';
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter_test/flutter_test.dart";
-import "package:recipe_suggestion/firebase_options.dart";
-import "package:recipe_suggestion/main.dart";
-import 'package:mockito/mockito.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:mockito/annotations.dart';
-import 'package:recipe_suggestion/utils/log.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:recipe_suggestion/view/function_list_page.dart';
-import 'function_list_page_test.mocks.dart';
 import 'mock.dart';
 
-@GenerateMocks([Log, FirebaseAnalytics])
 void main() async{
-  // setUp(() async{
-  //   TestWidgetsFlutterBinding.ensureInitialized();
-  //   WidgetsFlutterBinding.ensureInitialized();
-  //   setupFirebaseCoreMocks;
-  //   // await Firebase.initializeApp(
-  //   //   // options: DefaultFirebaseOptions.currentPlatform,
-  //   // );
-  //   await Firebase.initializeApp();
-  // });
   Widget createWidgetForTesting({required Widget child}) {
     return MediaQuery(
         data: const MediaQueryData(),
@@ -34,8 +16,8 @@ void main() async{
     setupFirebaseAuthMocks();
     await Firebase.initializeApp();
 
-    var log = MockLog();
     await widgetTester.pumpWidget(createWidgetForTesting(child: const ProviderScope(child: FunctionListPage(),)));
+    /// 画面表示文字列の確認
     expect(find.text("機能一覧"), findsOneWidget);
   });
 }
