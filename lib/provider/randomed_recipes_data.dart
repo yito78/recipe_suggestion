814e0adb3eb90a7889706e2dcadc954de3262184
yosukeito <yosuke.ito@uniadex.co.jp>
2023-06-22 08:28:14 +0000

    issue #47 1週間レシピ画面のデータ表示の関連処理にタイプセーフ化したデータを適用

--- lib/provider/randomed_recipes_data.dart
+++ lib/provider/randomed_recipes_data.dart
@@ -1,3 +1,4 @@
+import 'package:recipe_suggestion/data/recipe.dart';
 import 'package:recipe_suggestion/utils/weekly_recipe.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:recipe_suggestion/domain/repository/firebase.dart';
@@ -10,18 +11,22 @@ class RandomedRecipesDataNotifier extends _$RandomedRecipesDataNotifier {
   @override
   // 初期化処理
   Future<Map<int, List<dynamic>>> build() async {
-    List<Map<String, dynamic>> recipeData = await _fetchAllRecipesData();
+    // List<Map<String, dynamic>> recipeData = await _fetchAllRecipesData();
+    List<Recipe> recipeData = await _fetchAllRecipesDataTypeSafe();
     WeeklyRecipe weeklyRecipe = WeeklyRecipe();
-    Future<Map<int, List<dynamic>>> stateData = weeklyRecipe.createWeeklyRecipe(recipeData);
+    Future<Map<int, List<dynamic>>> stateData =
+        weeklyRecipe.createWeeklyRecipe(recipeData);
 
     return stateData;
   }
 
   // レシピデータ更新処理
-  fetchRandomedRecipeDataState() async{
-    List<Map<String, dynamic>> recipeData = await _fetchAllRecipesData();
+  fetchRandomedRecipeDataState() async {
+    // List<Map<String, dynamic>> recipeData = await _fetchAllRecipesData();
+    List<Recipe> recipeData = await _fetchAllRecipesDataTypeSafe();
     WeeklyRecipe weeklyRecipe = WeeklyRecipe();
-    AsyncValue<Map<int, List<dynamic>>> stateData = AsyncValue.data(await weeklyRecipe.createWeeklyRecipe(recipeData));
+    AsyncValue<Map<int, List<dynamic>>> stateData =
+        AsyncValue.data(await weeklyRecipe.createWeeklyRecipe(recipeData));
 
     state = stateData;
   }
@@ -30,6 +35,13 @@ class RandomedRecipesDataNotifier extends _$RandomedRecipesDataNotifier {
   Future<List<Map<String, dynamic>>> _fetchAllRecipesData() async {
     Firebase firebase = Firebase();
     List<Map<String, dynamic>> data = await firebase.searchAllRecipes();
+    return data;
+  }
+
+  // レシピデータを全件取得
+  Future<List<Recipe>> _fetchAllRecipesDataTypeSafe() async {
+    Firebase firebase = Firebase();
+    List<Recipe> data = await firebase.searchAllRecipesTypeSafe();
     return data;
   }
 }
