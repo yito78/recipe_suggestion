99dc54c0fb14ad1dd69f738ced26e51082b68222
yosukeito <yosuke.ito@uniadex.co.jp>
2023-07-19 15:18:18 +0000

    issue #58 drawer画面のレイアウトを調整

--- lib/view/drawer_page.dart
+++ lib/view/drawer_page.dart
@@ -6,11 +6,25 @@ class DrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      shadowColor: Colors.black26,
       width: MediaQuery.of(context).size.width * 0.5,
-      child: ListView(
-        children: const [
-          Text("ログアウト"),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: const [
+            Text(
+              "オプションメニュー",
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Text("ログアウト"),
+            )
+          ],
+        ),
       ),
     );
   }
