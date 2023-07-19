f995b9c261e0d81486e2389e2a0c92acf3b58ba2
yosukeito <yosuke.ito@uniadex.co.jp>
2023-07-19 06:49:09 +0000

    issue #58 main処理でログイン状態を監視し、状態に応じて遷移先画面を変更する処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:recipe_suggestion/provider/login_user.dart';
+import 'package:recipe_suggestion/view/function_list_page.dart';
 import 'package:recipe_suggestion/view/login_page.dart';
 import 'firebase_options.dart';
 
@@ -13,28 +15,24 @@ void main() async {
   runApp(scope);
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final signedInUserWatch = ref.watch(userProvider);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Recipe Suggestion',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      // ログイン済みであれば、機能一覧画面へ遷移する
+      home: signedInUserWatch.value != null
+          ? const FunctionListPage()
+          : const LoginPage(),
     );
   }
 }
--- lib/provider/login_user.dart
+++ lib/provider/login_user.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:recipe_suggestion/domain/repository/firebase_authentication.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 part 'login_user.g.dart';
 
 @riverpod
