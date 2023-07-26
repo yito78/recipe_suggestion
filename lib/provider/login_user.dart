992e86cac843152b2516f3b23d37854db8fcc6b7
yosukeito <yosuke.ito@uniadex.co.jp>
2023-07-26 02:43:04 +0000

    issue #58 PR指摘事項対応 login_user不要コードを削除

--- lib/provider/login_user.dart
+++ lib/provider/login_user.dart
@@ -1,19 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:recipe_suggestion/domain/repository/firebase_authentication.dart';
-part 'login_user.g.dart';
-
-@riverpod
-// Notifierクラス
-class LoginUserNotifier extends _$LoginUserNotifier {
-  @override
-  // 初期化処理
-  User? build() {
-    FirebaseAuthentication firebaseAuthentication = FirebaseAuthentication();
-    return firebaseAuthentication.fetchSignedInUser();
-  }
-}
 
 final currentUser = Provider<User>((ref) => throw UnimplementedError());
 
