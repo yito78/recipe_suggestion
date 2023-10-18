1360216fa9f1cf23b53afcb84deeb51109eb0221
yosukeito <yosuke.ito@uniadex.co.jp>
2023-10-18 13:58:42 +0000

    issue #68 _calclateDateForListメソッドの処理最適化およびコメント修正

--- lib/utils/weekly_recipe.dart
+++ lib/utils/weekly_recipe.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
+
 ///
 /// 1週間レシピ一覧画面に表示するレシピ情報を作成する
 ///
@@ -221,23 +223,20 @@ class WeeklyRecipe {
 
   ///
   /// 基準曜日(アプリ利用日)をもとに該当週の日付を計算する
-  /// 計算対象曜日が基準曜日以前である場合、基準曜日の日付から減算する
-  /// 逆の場合は、基準曜日の日付に対して加算する
+  ///   1. 計算対象曜日から基準曜日を減算する
+  ///   2. アプリ利用日に1の結果を加算する
   ///
   ///   以下表は、baseWeekdayを2(火曜日)とした場合の日付算出式を一覧にしたものとなる
   ///
   ///   baseWeekday | targetWeekday | 算出式
   ///   ----------------------------------------------------
-  ///    2(火曜日)   | 1(月曜日)      | アプリ利用日 - (2(baseWeekday) - 1(targetWeekday))
-  ///    2          | 2(火曜日)      | アプリ利用日 - (2(baseWeekday) - 2(targetWeekday))
-  ///    2          | 3(水曜日)      | アプリ利用日 + (3(targetWeekday) - 2(baseWeekday)) ※
+  ///    2(火曜日)   | 1(月曜日)      | アプリ利用日 + (1(targetWeekday) - 2(baseWeekday))
+  ///    2          | 2(火曜日)      | アプリ利用日 + (2(targetWeekday) - 2(baseWeekday))
+  ///    2          | 3(水曜日)      | アプリ利用日 + (3(targetWeekday) - 2(baseWeekday))
   ///    2          | 4(木曜日)      | アプリ利用日 + (4(targetWeekday) - 2(baseWeekday))
   ///    2          | 5(金曜日)      | アプリ利用日 + (5(targetWeekday) - 2(baseWeekday))
   ///    2          | 6(土曜日)      | アプリ利用日 + (6(targetWeekday) - 2(baseWeekday))
   ///    2          | 7(日曜日)      | アプリ利用日 + (7(targetWeekday) - 2(baseWeekday))
-  ///
-  ///   ※行で、baseWeekday(アプリ利用日)よりtargetWeekday(計算対象曜日)を上回り、
-  ///   アプリ利用日に対して加算を行うため、式を反転させる
   ///
   /// [datetime] アプリ利用日となり、曜日情報をもとに各曜日の日付を算出するために利用する
   /// [targetWeekday] 計算対象曜日(月曜なら1, 火曜なら2…)
@@ -247,13 +246,7 @@ class WeeklyRecipe {
   ///
   String _calclateDateForList(
       DateTime datetime, int targetWeekday, int baseWeekday) {
-    if (baseWeekday >= targetWeekday) {
-      DateTime date =
-          datetime.subtract(Duration(days: baseWeekday - targetWeekday));
-      return "${date.year}${date.month}${date.day}";
-    } else {
-      DateTime date = datetime.add(Duration(days: targetWeekday - baseWeekday));
-      return "${date.year}${date.month}${date.day}";
-    }
+    DateTime date = datetime.add(Duration(days: targetWeekday - baseWeekday));
+    return "${date.year}${date.month}${date.day}";
   }
 }
